36d88724184d0e905b02e02b2f2a9d010d72087f
Aguada, Darren Michael <darren.m.aguada@accenture.com>
2021-10-29 14:51:57 +0800

    minor changes

--- vars/inputParameters.groovy
+++ vars/inputParameters.groovy
@@ -5,17 +5,17 @@ def call() {
          name: 'environment', 
          defaultValue: 'terraform', 
          description: 'Workspace/environment file to use for deployment'
-      )
+      ),
       booleanParam(
          name: 's3Bucket',
          defaultValue: false, 
          description: 'Create s3 bucket for backend?'
-      )
+      ),
       booleanParam(
          name: 'autoApprove', 
          defaultValue: false, 
          description: 'Automatically run apply after generating plan?'
-      )
+      ),
       booleanParam(
          name: 'destroy',
          defaultValue: false,
